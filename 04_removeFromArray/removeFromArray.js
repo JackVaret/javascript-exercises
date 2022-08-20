d9bf707d2b48c4fb29352bc47907b263b4982566
JackVaret <victorogunsanya13@gmail.com>
2022-08-20 18:22:43 -0400

    completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(array,...args) {
+    // Set up an empty array to be returned later
+    let removedArray = []
+    // Create a for loop throughout the entire original array
+    for(let i =0;i<array.length;i++)
+    {
+        // Check if an element within that array is an element that needs to be removed
+        if(args.includes(array[i]))
+        {
+            // If the element at that value needs to be removed, do not copy it to removedArray
+            continue;
+        }
+         // Otherwise, copy the elements of the old array to the new array
+         removedArray.push(array[i]);
+         console.log(removedArray);
+    }
+    
+       
+        
+         
+        
+       
+        
+    return removedArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
