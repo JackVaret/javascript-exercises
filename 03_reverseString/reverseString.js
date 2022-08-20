372ba793af981fdccbfaa40e86aedd2d64fbe68a
JackVaret <victorogunsanya13@gmail.com>
2022-08-20 17:18:51 -0400

    Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Create an empty string to store new reversed string
+    let reversedString = '';
+    // Write for loop from the ending of the original string up to zero
+    for(let i = string.length -1; i>=0;i--)
+    {
+        reversedString+= string[i];
+    }  
+    return reversedString;
 };
 
 // Do not edit below this line
