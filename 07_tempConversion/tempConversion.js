9ef23a6cfb284ac88139ca8b57b7e49b50ac9d31
JackVaret <victorogunsanya13@gmail.com>
2022-08-20 19:01:31 -0400

    Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,25 @@
-const ftoc = function() {
+const ftoc = function(temperature) {
+  // Create a variable for the final temperature
+  let convertedTemperature = 0;
 
+  // Convert the tempereature from fahrenheit to celsius
+  convertedTemperature += ((temperature - 32) * (5/9))
+  // Round the converted temperature to one decimal place
+  convertedTemperature = Math.round(convertedTemperature*10)/10;
+
+  return convertedTemperature;
 };
 
-const ctof = function() {
+const ctof = function(temperature) {
+  // Create a variable for the final temperature
+  let convertedTemperature = 0;
+  // Convert the temperature from celsius to fahrenheit
+  convertedTemperature += ((temperature * (9/5)) + 32)
+  // Round the converted temperature to one decimal place
+  convertedTemperature = Math.round(convertedTemperature*10)/10;
 
+  //Return converted temperature
+  return convertedTemperature;
 };
 
 // Do not edit below this line
