6e4d60ee57628bc52c097d29d9f24cd837746580
JackVaret <victorogunsanya13@gmail.com>
2022-08-20 18:50:51 -0400

    completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,33 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Check to see if a year is divisible by 100
+    if(year %100==0)
+    {
+        // Check to see if a year divisible by 100 is divisible by 400
+        if(year %400==0)
+        {
+            // If true, return true
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    // Check to see if the year is not divisible by 100
+    else if(year%100 !=0)
+    {
+        // Check to see if the year is divisble by 4
+        if(year%4==0)
+        {
+            // Return true
+            return true;
+        }
+        else
+        {
+            //Return false
+            return false;
+        }
+    }
 };
 
 // Do not edit below this line
